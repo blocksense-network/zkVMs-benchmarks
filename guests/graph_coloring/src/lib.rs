c5cc00e84ab366b57c80ed7804c398ea7ecefc98
Kamen Mladenov <kamen@syndamia.com>
2025-01-09 14:43:31 +0200

    feat(guests): Add graph_coloring program

---
+++ guests/graph_coloring/src/lib.rs
@@ -0,0 +1,28 @@
+const VERTICES: usize = 010;
+
+#[guests_macro::proving_entrypoint]
+pub fn start(
+    graph: Vec<Vec<bool>>,
+    colors: u32,
+    coloring: Vec<Vec<u32>>,
+) -> bool {
+    // Does it use the correct amount of colors?
+    let mut max_color = coloring[0][1];
+    for nc in &coloring {
+        if nc[1] > max_color {
+            max_color = nc[1];
+        }
+    }
+
+    let mut ret = max_color + 1 == colors;
+
+    // Is coloring correct?
+    for i in 0..VERTICES {
+        for j in 0..VERTICES {
+            // graph[i][j] -> coloring[i] != coloring[j]
+            ret = ret & (! graph[i][j] | (coloring[i][1] != coloring[j][1]));
+        }
+    }
+
+    ret
+}
