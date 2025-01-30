492467015ec8a339eefd816a818e73cbc3fcece8
Kamen Mladenov <kamen@syndamia.com>
2025-01-30 13:09:53 +0200

    feat(guests): Add a second example guest

---
+++ guests/fibonacci/src/lib.rs
@@ -0,0 +1,15 @@
+#![cfg_attr(feature = "no_std", no_std)]
+
+#[guests_macro::proving_entrypoint]
+pub fn main(n: u8, fN: u64) -> bool {
+    let mut f0 = 0;
+    let mut f1 = 1;
+
+    for _ in 1..n {
+        let fN = f0 + f1;
+        f0 = f1;
+        f1 = fN;
+    }
+
+    f0 == fN
+}
