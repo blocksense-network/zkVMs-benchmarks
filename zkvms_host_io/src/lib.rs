c316d30af44a54119a168967d591b0e8ec4d54c3
Kamen Mladenov <kamen@syndamia.com>
2025-01-31 10:13:28 +0200

    feat(zkvms_host_io): Add default_env RunWith attribute

--- zkvms_host_io/src/lib.rs
+++ zkvms_host_io/src/lib.rs
@@ -1,10 +1,12 @@
 use clap::{Parser, ValueEnum};
 use num_traits::NumCast;
 use serde::{ Serialize, Deserialize };
-use std::fs::read_to_string;
+use env_file_reader::read_str;
+use std::{ fs::read_to_string, collections::HashMap };
 pub use input_macros::foreach_input_field;
 
 static DEFAULT_PUBLIC_INPUT: &str = include_str!(concat!(env!("INPUTS_DIR"), "/default_public_input.toml"));
+static DEFAULT_ENV: &str = include_str!(concat!(env!("INPUTS_DIR"), "/default.env"));
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -22,10 +24,11 @@ pub enum RunType {
     Verify,
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct RunWith<T> {
     pub run_type: RunType,
     pub input: T,
+    pub default_env: HashMap<String, String>,
 }
 
 input_macros::generate_input_struct!();
@@ -38,10 +41,13 @@ pub fn read_args() -> RunWith<Input> {
         } else {
             DEFAULT_PUBLIC_INPUT.to_string()
         };
-    let val: Input = toml::from_str(&contents).unwrap();
+    let input: Input = toml::from_str(&contents).unwrap();
+
+    let default_env = read_str(DEFAULT_ENV).unwrap();
 
     RunWith {
         run_type: cli.run_type,
-        input: val,
+        input,
+        default_env,
     }
 }
