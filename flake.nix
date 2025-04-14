230defc3f58161234f8752e98ecf11725c85860d
Kamen Mladenov <kamen@syndamia.com>
2025-04-14 09:45:51 +0300

    feat(update-nix-dependencies): Use flake's nix-blockchain-development instead of default
    To get the new Nix outPaths for all zkVMs, we used to execute a `nix
    run` command, which found the paths from the nix-blockchain-development
    repo, default branch on latest commit.
    
    Since that could differ from what repo our flake actually uses, we're
    updating the logic.

--- flake.nix
+++ flake.nix
@@ -74,8 +74,11 @@
         }) { } guests;
     in {
       packages.${system} = hostPackages // guestPackages // {
-        rust-format-all = callPackage ./rust-format-all.nix { };
-        update-nix-dependencies = callPackage ./update-nix-dependencies.nix { };
+        rust-format-all = callPackage ./rust-format-all.nix {
+        };
+        update-nix-dependencies = callPackage ./update-nix-dependencies.nix {
+          zkvms = builtins.map (name: mcl-blockchain.packages.${system}.${name}) zkvms;
+        };
       };
 
       formatter.${system} = pkgs.nixfmt;
