edd398d678fb34629e08d23b08080d105fb2ec79
Kamen Mladenov <kamen@syndamia.com>
2025-04-14 11:51:36 +0300

    feat(guests/keccak): Add ZKM precompile function call

--- guests/keccak/src/lib.rs
+++ guests/keccak/src/lib.rs
@@ -5,13 +5,22 @@ extern crate alloc;
 #[cfg(feature = "no_std")]
 use alloc::vec::Vec;
 
+#[cfg(feature = "zkm")]
+use zkm_runtime::*;
+
 use sha3::{Digest, Keccak256};
 
 #[guests_macro::proving_entrypoint]
 pub fn main(secret: Vec<u8>, hash: Vec<u8>) -> bool {
-    let mut hasher = Keccak256::new();
-    hasher.update(secret);
-    let result = hasher.finalize();
+    #[cfg(feature = "zkm")]
+    let result = zkm_runtime::io::keccak(&secret.as_slice());
+
+    #[cfg(not(any(feature = "zkm")))]
+    let result = {
+        let mut hasher = Keccak256::new();
+        hasher.update(secret);
+        hasher.finalize()
+    };
 
     let output: [u8; 32] = result.into();
 
