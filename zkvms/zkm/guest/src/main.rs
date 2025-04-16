764818f5d7222f65bc4bf810dcf48360d14602f9
Kamen Mladenov <kamen@syndamia.com>
2025-04-16 09:46:36 +0300

    fix(zkvms/zkm): Include string in guest wrapper

--- zkvms/zkm/guest/src/main.rs
+++ zkvms/zkm/guest/src/main.rs
@@ -4,7 +4,7 @@
 use wrapper_macro::make_wrapper;
 
 extern crate alloc;
-use alloc::{collections::*, vec::*};
+use alloc::{collections::*, vec::*, string::*};
 use zkm_runtime::io::{commit, read};
 
 zkm_runtime::entrypoint!(main);
